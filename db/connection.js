3ec93b2da49e6ae98e4fe49f5e3609083747c310
nathan-pj <nathan.pagejones@gmail.com>
2022-01-09 16:08:18 +0000

    removed logs

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,6 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
-console.log(process.env.NODE_ENV);
-console.log(ENV);
+
 const config =
   ENV === "production"
     ? {
