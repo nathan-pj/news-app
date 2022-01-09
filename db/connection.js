247624a5e18e7ab83467169e5d7ffb9d5aace316
nathan-pj <nathan.pagejones@gmail.com>
2022-01-09 16:19:30 +0000

    trial

--- db/connection.js
+++ db/connection.js
@@ -13,6 +13,7 @@ const config =
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
+console.log(process.env.PGDATABASE, "<<<<<<<<<<<<<<<<<<<<<<");
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE not set or DATABASE_URL not set");
