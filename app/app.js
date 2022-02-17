f07eef38707143290b5d04bd7e8df154239e22d5
nathan-pj <nathan.pagejones@gmail.com>
2022-02-17 16:08:24 +0000

    cors

--- app/app.js
+++ app/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 const {
   handleCustomErrors,
   handlePsqlErrors,
@@ -9,7 +14,7 @@ const {
 const apiRouter = require("./routes/api.router");
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api", apiRouter);
